5fd1cc5a525450be72622657256372c27368916b
Luca Schirmbrand <luca.schirmbrand@hotmail.com>
2022-06-29 11:27:33 +0200

    update

--- snapshots/exec.py
+++ snapshots/exec.py
@@ -1,11 +1,11 @@
 import cv2
-camera = cv2.VideoCapture(1)
+camera = cv2.VideoCapture(0)
 while True:
     return_value,image = camera.read()
-    gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-    cv2.imshow('image',gray)
+    # gray = cv2.cvtColor(image)
+    cv2.imshow('image',image)
     if cv2.waitKey(1)& 0xFF == ord('s'):
-        cv2.imwrite('test.jpg',image)
+        cv2.imwrite('./snapshots/results/test.jpg',image)
         break
 camera.release()
 cv2.destroyAllWindows()
