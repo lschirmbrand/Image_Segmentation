1cac80f556e55e2e790a99d0926d884b5271e7af
Schirmbrand <lschirmbrand@mlog.ads>
2022-08-11 14:01:10 +0200

    Update exec.py

--- bin_to_ply/exec.py
+++ bin_to_ply/exec.py
@@ -1,6 +1,7 @@
 import copy
 import math
 import sys
+from tracemalloc import start
 import numpy as np
 import struct
 import open3d as o3d
@@ -41,10 +42,10 @@ lines = [[0, 1], [0, 2], [0, 3]]
 # colors = [[1, 0, 0] for i in range(len(lines))]
 colors = [[1, 0, 0], [0,1,0], [0,0,1]]
 
-line_set = o3d.geometry.LineSet()
-line_set.points = o3d.utility.Vector3dVector(points)
-line_set.lines = o3d.utility.Vector2iVector(lines)
-line_set.colors = o3d.utility.Vector3dVector(colors)
+lineset = o3d.geometry.LineSet()
+lineset.points = o3d.utility.Vector3dVector(points)
+lineset.lines = o3d.utility.Vector2iVector(lines)
+lineset.colors = o3d.utility.Vector3dVector(colors)
 
 
 def translate(pcd, translation):
@@ -111,6 +112,8 @@ def rotate_to_normal(pcd_normal, plane_normal):
     costheta = np.dot(pcd_normal,plane_normal)/(np.linalg.norm(pcd_normal)*np.linalg.norm(plane_normal))
     axis = unitcross(pcd_normal, plane_normal)
 
+    print(axis)
+
     c = costheta
     C = 1 - c
     s = np.sqrt(1 - c**2)
@@ -154,10 +157,76 @@ def rotate_to_center_plane(pcd):
     inlier_cloud.paint_uniform_color([1.0, 0, 0])
     return pcd, inlier_cloud
 
-def apply_rotation(pcd, R, center):
+def apply_rotation_pcd(pcd, R, center):
     pcd.rotate(R, center=center)
     return pcd
 
+def calculate_center(points3d):
+    return np.mean(points3d, axis=0)
+
+def get_max_bounds(points3d, variance=0.01):
+    for p in points3d:
+        for i, p2 in enumerate(points3d):
+            if p is p2:
+                continue
+            else:
+                if(abs(p[0] - p2[0]) < variance and abs(p[1] - p2[1]) < variance and abs(p[2] - p2[2]) < variance):
+                    p[0] = p2[0]
+                    p[1] = p2[1]
+                    p[2] = p2[2]
+
+    points3d = np.unique(points3d, axis=0)
+    
+    if(len(points3d) != 4):
+        print("Error: not enough points")
+        return None
+    else: return points3d
+
+
+def get_outer_cameras(cameras, points3d):
+    outer_cameras = []
+    for point in points3d:
+        distances = []
+
+        for i, cam in enumerate(cameras):
+            distances.append(calculate_distance(point, cam))
+        
+        min_dis = min(distances)
+        min_index = distances.index(min_dis)
+        outer_cameras.append(cameras[min_index])
+        # cameras.remove(cameras[min_index])
+    return outer_cameras
+    
+
+def calculate_distance(p1, p2):
+    return np.linalg.norm(p1-p2)
+
+def get_smallest_distance(points, p):
+    distances = []
+    for point in points:
+        distances.append(calculate_distance(p, point))
+    return min(distances), distances.index(min(distances))
+
+def get_highest_distance(points, p):
+    distances = []
+    for point in points:
+        distances.append(calculate_distance(p, point))
+    return max(distances), distances.index(max(distances))
+
+def calculate_vector(p1, p2):
+    return p2-p1
+
+def apply_translation_to_points(translation, points):
+    for i, point in enumerate(points):
+        points[i] = point + translation
+    return points
+
+def apply_rotation_to_points(P, R):
+    return np.asarray(np.dot(P, R.T))
+
+
+def rearrange_cloud():
+    pass
 
 #  REFFERENCE OBJECT
 
@@ -167,14 +236,67 @@ center = pcd_reference.get_center()
 cameras_ref = init_cameras('bin_to_ply/cameras_ref.txt')
 cameras_ref_points = get_all_camera_positions(cameras_ref)
 
+cam_ref_center = calculate_center(cameras_ref_points)
+# print(cam_ref_center)
+
 camera_ref_pcd = o3d.geometry.PointCloud()
 camera_ref_pcd.points = o3d.utility.Vector3dVector(cameras_ref_points)
 
-camera_ref_plane, plane_model = generate_plane(camera_ref_pcd, distance_threshold=100, ransac_n=len(cameras_ref))
-camera_plane_ref_R = get_rotation_matrix(plane_model, [0,1,0])
+camera_ref_pcd = translate(camera_ref_pcd, -cam_ref_center)
 
-[a,b,c,d] = plane_model
-print(f"Plane equation cam: {a:.4f}x + {b:.4f}y + {c:.4f}z + {d:.4f} = 0")
+cam_ref_center = camera_ref_pcd.get_center()
+
+cam_ref_center_pcd = o3d.geometry.PointCloud()
+cam_ref_center_pcd.points = o3d.utility.Vector3dVector([cam_ref_center])
+
+cam_ref_pcd_bb = camera_ref_pcd.get_oriented_bounding_box()
+corner_points = np.asarray(cam_ref_pcd_bb.get_box_points())
+
+corner_points = get_max_bounds(corner_points, variance=0.2)
+outer_cams = get_outer_cameras(np.asarray(camera_ref_pcd.points), corner_points)
+
+print(outer_cams)
+
+_, index_start = get_smallest_distance(outer_cams, (0,0,0))
+print(index_start)
+start_cam = outer_cams[index_start]
+outer_cams = np.delete(outer_cams, index_start, axis=0)
+
+_, index_remove = get_highest_distance(outer_cams, start_cam)
+outer_cams = np.delete(outer_cams, index_remove, axis=0)
+
+cam_vector_points = np.concatenate((start_cam.reshape(1,3), outer_cams), axis=0)
+
+t = calculate_vector(cam_vector_points[0], (0,0,0))
+cam_vector_points = apply_translation_to_points(t, cam_vector_points)
+camera_ref_pcd.translate(t, relative=True)
+
+print(cam_vector_points)
+R1 = rotate_to_normal(calculate_vector(cam_vector_points[0], cam_vector_points[1]), [1,0,0])
+cam_vector_points = apply_rotation_to_points(cam_vector_points, R1)
+
+vec = calculate_vector(cam_vector_points[0], cam_vector_points[2])
+vec[0] = 0
+# print("vec ", vec)
+print(cam_vector_points)
+R2 = rotate_to_normal(vec, [0,0,1])
+
+cam_vector_points = apply_rotation_to_points(cam_vector_points, R2)
+
+camera_ref_pcd.rotate(R1, center=(0,0,0))
+camera_ref_pcd.rotate(R2, center=(0,0,0))
+
+pcd_reference.translate(t, relative=True)
+pcd_reference.rotate(R1, center=(0,0,0))
+pcd_reference.rotate(R2, center=(0,0,0))
+
+print("cam_vector_points \n", cam_vector_points)
+
+cam_vectors = o3d.geometry.LineSet()
+cam_vectors.points = o3d.utility.Vector3dVector(cam_vector_points)
+cam_vectors.lines = o3d.utility.Vector2iVector([[0,1], [0,2], [1,2]])
+cam_vectors.colors = o3d.utility.Vector3dVector([[1, 0, 0]])
+
 
 #  TEST OBJECT
 cameras_object = init_cameras('bin_to_ply/cameras_test.txt')
@@ -200,13 +322,13 @@ R = camera_plane_ref_R @ pcd_reference_R
 # pcd_ref_rotated_2 = apply_rotation(copy.deepcopy(pcd_reference), pcd_reference_R, (0,0,0))
 # pcd_ref_rotated_3 = apply_rotation(copy.deepcopy(pcd_reference), pcd_reference_R, (0,0,0))
 # pcd_ref_rotated_3 = apply_rotation(copy.deepcopy(pcd_reference), camera_plane_R, (0,0,0))
-pcd_ref_rotated = apply_rotation(copy.deepcopy(pcd_reference), R, (0,0,0))
+pcd_ref_rotated = apply_rotation_pcd(copy.deepcopy(pcd_reference), R, (0,0,0))
 bb_pcd_reference = o3d.geometry.OrientedBoundingBox.create_from_points(pcd_ref_rotated.points)
 
 
 
 # o3d.visualization.draw_geometries([bb_ref, bb_oversized, ref_in, over_in, pcd_coordinate_system, pcd_reference, pcd_oversized, line_set])
-o3d.visualization.draw_geometries([line_set, pcd_reference, camera_ref_pcd, camera_test_pcd, pcd_testobject])
+o3d.visualization.draw_geometries([cam_vectors, camera_ref_pcd, cam_ref_center_pcd, cam_vectors, lineset, pcd_reference])
 # o3d.visualization.draw_geometries([ref_in, bb_ref, pcd_coordinate_system, pcd_reference, line_set])
 # o3d.visualization.draw_geometries([bb, pcd_coordinate_system, pcd_reference, line_set])
 # o3d.visualization.draw_geometries([pcd_coordinate_system, pcd_reference, pcd_oversized, line_set])
